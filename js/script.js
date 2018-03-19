0cf3bfa7132f1d8db60d9308f8a95408ab7f2914
joeyjim <>
2018-03-19 14:52:35 +0100

    v3
    v3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-function showtime() {// We create a new Date object and assign it to a variable called "time".
+var moon = document.getElementById('moon');
+var sun = document.getElementById('sun');
+var ufo = document.getElementById('ufo');
+var time = 3.5;
+
+//TweenMax.from(ufo, time, {css:{top: "-750px", scaleX:.1, scaleY:.1}, ease:Strong.easeInOut, delay: 1.5});
+
+var tl1 = new TimelineMax({repeat:-1});
+tl1.to(moon, 10, {top: "500px", ease: Sine.easeInOut})
+   .to(moon, 10, {left: "500px", ease: Sine.easeInOut} )
+   .to(moon, 10, {top: 8, ease: Sine.easeInOut} )
+   .to(moon, 10, {left: 8, ease: Sine.easeInOut} );
+
+var tl2 = new TimelineMax({repeat:-1});
+tl2.to(sun, 10, {bottom: "250px", ease: Sine.easeInOut})
+   .to(sun, 10, {right: "500px", ease: Sine.easeInOut})
+   .to(sun, 10, {bottom: 8, ease: Sine.easeInOut})
+   .to(sun, 10, {right: 8, ease: Sine.easeInOut});
+
+var tl3 = new TimelineMax({repeat:-1});
+tl3.from(ufo, time, {css:{top: "-750px", scaleX:.1, scaleY:.1}, ease:Strong.easeInOut, delay: 1.5})
+    .to(ufo, 5, {left: 6000, ease: Sine.easeInOut});  
+
+function showtime() {
     var date = new Date();
     var h = date.getHours();
     var m = date.getMinutes();
@@ -7,15 +30,21 @@ function showtime() {// We create a new Date object and assign it to a variable 
     var mo = date.getMonth()+1;
     var d = date.getDate();
     
-    var session = "ochtend";
-    
-  
-    
-    if(h > 12){
-       
-        session = "middag";
+    var session = "dag";
+ 
+    if(h < 8 && h > 16 ){
+        session = "nacht";
+        moon.style.visibility = "visible";
+        sun.style.visibility = "hidden";
+        
+    } else{
+        session = "dag";
+        moon.style.visibility = "hidden";
+        sun.style.visibility = "visible";
     }
     
+    d = (d < 10) ? "0" + d : d;
+    mo = (mo < 10) ? "0" + mo : mo;
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
@@ -30,17 +59,3 @@ function showtime() {// We create a new Date object and assign it to a variable 
    showtime();
 
 
-var moon = document.getElementById('moon');
-var sun = document.getElementById('sun');
-
-var tl1 = new TimelineMax({repeat:-1});
-tl1.to(moon, 2, {top: 250, ease: Sine.easeInOut})
-   .to(moon, 2, {left: 500, ease: Sine.easeInOut}, '-=0.25')
-   .to(moon, 2, {top: 8, ease: Sine.easeInOut}, '-=0.25')
-   .to(moon, 2, {left: 8, ease: Sine.easeInOut}, '-=0.25');
-
-var tl2 = new TimelineMax({repeat:-1});
-tl2.to(sun, 2, {bottom: 250, ease: Sine.easeInOut})
-   .to(sun, 2, {right: 500, ease: Sine.easeInOut}, '-=0.25')
-   .to(sun, 2, {bottom: 8, ease: Sine.easeInOut}, '-=0.25')
-   .to(sun, 2, {right: 8, ease: Sine.easeInOut}, '-=0.25');
